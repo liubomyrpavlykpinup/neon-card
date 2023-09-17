6d4ba38305d204c344c5e0d04f8159ed3dca13a3
veruyuschiyateist <lybomir.pavlick@gmail.com>
2023-09-17 20:31:11 +0300

    Initial commit

---
+++ app/src/main/java/com/tocaboca/tocacar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tocaboca.tocacar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
